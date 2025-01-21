548773b9d9023532f612b428a2fc34b2985eb915
KingAzule123 <c01227711@gmail>
2025-01-21 17:58:46 -0500

    pushing first unity project

---
+++ c#_1/firstScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firstScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int playerhealth = 100;
+
+    
+    void Start()
+    {
+        Debug.Log(playerhealth);
+        
+    }
+}
+// declare public integer named "playerhealth" and initalize it to 100.
+   
