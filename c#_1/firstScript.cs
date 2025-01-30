a71b89d1d21523fd20795e59261dd37b3279fa64
KingAzule123 <c01227711@gmail>
2025-01-30 17:56:32 -0500

    pushing updated first script

--- c#_1/firstScript.cs
+++ c#_1/firstScript.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class firstScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private int var = 5;
     public int playerhealth = 100;
+    
+
+    bool isnosnow = true;
+
+    bool issnow = true;
+
+}
+
+
 
     
     void Start()
     {
-        Debug.Log(playerhealth);
+        string[] fruit = {"Apple","Tomato","Blueberry","Pear","Banana"};  
+        for (int jumps = 0; jumps < 20; jumps++)
+        {
+            //for every Jump, keep track and print the results
+            Debug.Log("jumps"+ jumps );
+            if(isnosnow) //if "isnosnow" true, print message 2
+            Debug.Log("lets go play outside!");
+            else if (issnow) 
+            Debug.Writeline("Lets stay inside today.");
+    //define a funtion named "Add" that takes two integers 'z' and 'x'.
+    int Add(int z, int x )
+
+        //inside the function, add the values 'z' and 'x' together and return the result.
+        return z + x;
+    
+    
+    //Call the "Add" function with the values of 6 and 4, and store the result in the 'result' varible.
+    int result = Add(6,4);
+
+    //return the value stored in the 'result' varible, which is the sum of 6 and 4 (10).
+    return result:            
         
-    }
+                 
+
+        }
+
+       
+    
+
+
+
+       
+  
 }
-// declare public integer named "playerhealth" and initalize it to 100.
+
+
+      
+
+            
+    
+    
+ 
+
    
